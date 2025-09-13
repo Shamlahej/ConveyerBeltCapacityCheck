2a40ee8ca5498bf4fc6b570b1f819c1f6e6991ce
shamlaessabahram <s236155@dtu.dk>
2025-09-13 22:02:14 +0200

    opdateret

---
+++ ConveyerBeltCapacityCheck/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("How many motors are carrying the packages?");
+var motors = int.Parse(Console.ReadLine());
+
+Console.WriteLine("How many kg of packages do we expect?");
+var weight = double.Parse(Console.ReadLine());
+
+var quotient = weight / motors;
+if (quotient <=5.6)
+{ Console.WriteLine("Yes! The conveyor belt can carry the packages."); }
+else
+{ Console.WriteLine("No. The conveyor belt cannot carry the packages."); }
